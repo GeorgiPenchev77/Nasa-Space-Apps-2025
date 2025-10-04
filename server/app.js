62a92ebd410b7728141c398313bda9ad418a9863
Georgi Penchev <georgy22092004@gmail.com>
2025-10-04 17:18:19 +0200

    #2: add endpoint

--- server/app.js
+++ server/app.js
@@ -11,22 +11,29 @@ app.use(express.json());
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-app.post("/api/analyze", async (req, res) => {
-  try {
-    const { text } = req.body;
+app.post("/api/simplify", async(req, res) => {
 
-    if (!text) return res.status(400).json({ error: "Missing 'text'" });
+try{
+  const text = req.body.text;
 
-    const model = genAI.getGenerativeModel({ model: "gemini-2.5-Pro"});
+  if(!text) {
+      return res.status(400).json({ error: "Query is required" });
+    }
 
-    const response = await model.generateContent(text);
-    const reply = response.response;
-    console.log(reply);
+  const model = genAI.getGenerativeModel({model: "gemini-2.5-flash"});
 
-  } catch (error) {
+  const prompt = "Explain in simpler terms(under 50 words) the following: " + text;
+
+  const result = await model.generateContent(prompt);
+  const responseText = result.response.text();
+  console.log(responseText);  
+  console.log(text);
+  }
+  catch(error){
     console.error("OpenAI API error:", error);
     res.status(500).json({ error: "Failed to process request" });
   }
+
 });
 
 app.listen(PORT, () => {
