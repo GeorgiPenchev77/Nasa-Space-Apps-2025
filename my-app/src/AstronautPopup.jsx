8e4c567039c6d2c02dd4bc14f950d431869e31a0
omar <omarmous@student.chalmers.se>
2025-10-05 12:04:18 +0200

    update chat button to open chatroom URL instead of a blank page

--- my-app/src/AstronautPopup.jsx
+++ my-app/src/AstronautPopup.jsx
@@ -39,7 +39,7 @@ export default function AstronautPopup() {
             
             {/* Chat button */}
             <button
-              onClick={() => window.open('about:blank', '_blank', 'noopener,noreferrer')}
+              onClick={() => window.open('/chatroom', '_blank', 'noopener,noreferrer')}
               style={{
                 marginTop: "15px",
                 padding: "10px 20px",
